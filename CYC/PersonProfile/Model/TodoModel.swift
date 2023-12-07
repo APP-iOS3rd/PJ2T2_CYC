74a4d342a6ed2a804a82b59e6c28cd8642ad6958
kmh5038 <144766297+kmh5038@users.noreply.github.com>
2023-12-07 16:04:10 +0900

    [Feat] SettingView

---
+++ CYC/PersonProfile/Model/TodoModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoModel.swift
+//  CYC
+//
+//  Created by 김명현 on 12/7/23.
+//
+
+import Foundation
+
+struct TodoModel: Identifiable {
+    let id = UUID()
+    let title: String
+    
+}
