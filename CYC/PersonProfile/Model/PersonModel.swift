79250b6f0495a5628b0910f7601871c464960227
ciu <112779139+kangciu@users.noreply.github.com>
2023-12-09 18:42:58 +0900

    [Feat] 프로필 버튼 누르면 링크로 이동하게 만듬

--- CYC/PersonProfile/Model/PersonModel.swift
+++ CYC/PersonProfile/Model/PersonModel.swift
@@ -13,12 +13,13 @@ struct Person: Identifiable {
     var subtitle: String
     var image: String
     var color: Color
+    var link: String
 }
 
 var PersonData: [Person] = [
-    Person(name: "Ciu", subtitle: "iOS Developer", image: "angry", color: .ciu),
-    Person(name: "MinChae", subtitle: "iOS Developer", image: "kiss_pink", color: .minchae),
-    Person(name: "Minyoung", subtitle: "iOS Developer", image: "beauty", color: .minyoung),
-    Person(name: "MyungHyun", subtitle: "iOS Developer", image: "dizzy", color: .myunhyun),
-    Person(name: "SungJin", subtitle: "iOS Developer", image: "music", color: .orange)
+    Person(name: "Ciu", subtitle: "iOS Developer", image: "angry", color: .ciu, link: "https://github.com/kangciu"),
+    Person(name: "MinChae", subtitle: "iOS Developer", image: "kiss_pink", color: .minchae, link: "https://github.com/Hminchae"),
+    Person(name: "Minyoung", subtitle: "iOS Developer", image: "beauty", color: .minyoung, link: "https://github.com/Mminy62"),
+    Person(name: "MyungHyun", subtitle: "iOS Developer", image: "dizzy", color: .myunhyun, link: "https://github.com/kmh5038"),
+    Person(name: "SungJin", subtitle: "iOS Developer", image: "music", color: .orange, link: "https://github.com/Hsungjin")
 ]
