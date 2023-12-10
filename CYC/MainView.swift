eb793e00160e3a46284fc6fb309cf490c1c4d0a7
ciu <112779139+kangciu@users.noreply.github.com>
2023-12-10 21:37:45 +0900

    [Fix] 메인 뷰 기어 이미지가 라이트 모드일 때 색상 변경되게 끔 수정함 + gearblack 이미지 파일 추가

--- CYC/MainView.swift
+++ CYC/MainView.swift
@@ -8,44 +8,48 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         NavigationStack {
-                ZStack {
-                    Color.bgColor.ignoresSafeArea(.all)
-                    ScrollView {
-                        VStack {
-                            // MARK: - 네비게이션 타이틀
-                            HStack {
-                                Image("logo")
+            ZStack {
+                Color.bgColor.ignoresSafeArea(.all)
+                ScrollView {
+                    VStack {
+                        // MARK: - 네비게이션 타이틀
+                        HStack {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 70)
+                            
+                            Spacer()
+                            
+                            
+                            NavigationLink(destination: SettingView()) {
+                                // MARK: 모드에 따라서 이미지 색상 변경
+                                Image(colorScheme == .dark ? "gear" : "gearblack")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 70)
-                                
-                                Spacer()
-                                
-                                
-                                NavigationLink(destination: SettingView()) {
-                                    Image("gear")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25)
-                                }
+                                    .frame(width: 25)
                             }
-                            .padding()
-                            
-                            // MARK: - 상단 텍스트
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text("Hi, Ciu")
-                                
-                                Text("오늘 커밋 잡쉈어?")
-                            }
-                            .font(.pretendardBold_25)
-                            .hSpacing(.leading)
-                            .padding(.leading, 20)
                         }
+                        .padding()
                         
-                        // MARK: - 하단 뷰 연결
-                        ProgressView()
+                        // MARK: - 상단 텍스트
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Hi, Ciu")
+                            
+                            Text("오늘 커밋 잡쉈어?")
+                        }
+                        .font(.pretendardBold_25)
+                        .hSpacing(.leading)
+                        .padding(.leading, 20)
+                    }
+                    
+                    // MARK: - 하단 뷰 연결
+                    ProgressView()
                 }
             }
         }
