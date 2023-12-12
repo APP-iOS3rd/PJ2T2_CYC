c65c9294c5195d5220e36c953f7889560ff11717
Hminchae <hminchae@gmail.com>
2023-12-12 14:57:28 +0900

    [Fix] 알람 설정 AppDelegate -> SettingView 로 옮기고 수정

--- CYC/Setting/View/NotificationView.swift
+++ CYC/Setting/View/NotificationView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NotificationView: View {
     
-    @State var isOnNotification = false
+    @AppStorage("notification") var isOnNotification: Bool = false
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var backButton : some View {  // <-- 👀 커스텀 버튼
@@ -37,7 +37,7 @@ struct NotificationView: View {
                     // MARK: - 알림 설정 토글
                     Text("알림 설정")
                         .font(.pretendardBold_25)
-                })
+                }).onChange(of: isOnNotification, initial: false, techNotification)
                 
                 // MARK: - 알림 설정 상세 설명
                 Text("오후 7시부터 12시까지 정각 시간마다 알림")
@@ -56,6 +56,52 @@ struct NotificationView: View {
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: backButton)
+    }
+    
+    func techNotification() {
+        let triggerDate = Calendar.current.date(byAdding: .minute, value: 1, to: Date())! // 예: 현재로부터 1분 후
+        
+        if isOnNotification {
+
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "6시인데.. 커밋 안 해요?",
+                                           hour: 18,
+                                           identifier: "SCHEDULED_NOTI")
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "커밋만 치고 푸시 안하신건 아니죠?",
+                                           hour: 19,
+                                           identifier: "SCHEDULED_NOTI")
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "커밋하고 성공시대 시작됐다. 에 듀 윌 !",
+                                           hour: 20,
+                                           identifier: "SCHEDULED_NOTI")
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "너의 커밋을 누르고..설레임에 푸쉬 누르다..",
+                                           hour: 21,
+                                           identifier: "SCHEDULED_NOTI")
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "치우랑 사귈래 커밋할래",
+                                           hour: 22,
+                                           identifier: "SCHEDULED_NOTI")
+            LocalNotificationHelper
+                .shared
+                .pushScheduledNotification(title: "Check Your Commit",
+                                           body: "성공하면 커밋 실패하면 반역",
+                                           hour: 23,
+                                           identifier: "SCHEDULED_NOTI")
+        } else {
+            LocalNotificationHelper.shared.removePendingNotification(identifiers: ["SCHEDULED_NOTI"])
+        }
     }
 }
 
