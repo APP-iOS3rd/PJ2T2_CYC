1ca817dc30847550564fd48258c25b6f089a7740
Minyoung Lee <mminy62@gmail.com>
2023-12-14 17:49:10 +0900

    [Fix] progressBar width 수정
    진짜 마지막.....

--- CYC/Progress/ProgressBarView.swift
+++ CYC/Progress/ProgressBarView.swift
@@ -18,7 +18,7 @@ struct ProgressBarView: View {
                 RoundedRectangle(cornerRadius: 60)
                     .foregroundColor(.bgColor)
                     .onAppear {
-                        containerWidth = UIScreen.main.bounds.width - 140
+                        containerWidth = UIScreen.main.bounds.width - 95
                     }
             }
             RoundedRectangle(cornerRadius: 60)
