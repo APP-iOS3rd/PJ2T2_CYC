1af702df5f8865579c3f4e1d75f241ccf32d1267
ciu <112779139+kangciu@users.noreply.github.com>
2023-12-15 12:35:07 +0900

    [Edit] SettingView 수정 및 Theme Setting 변경

--- CYC/Setting/View/NotificationView.swift
+++ CYC/Setting/View/NotificationView.swift
@@ -51,18 +51,16 @@ struct NotificationView: View {
                         .font(.pretendardBold_25)
                 }).onChange(of: isOnNotification, initial: false, techNotification)
                 
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 8) {
                     // MARK: - 알림 설정 상세 설명
                     Text("오후 7시부터 12시까지 정각 시간마다 알림")
                         .font(.pretendardSemiBold_15)
                         .padding(.top, 15)
                     Text("자정 전에 커밋을 하면 알람이 자동으로 종료됩니다")
                         .font(.pretendardSemiBold_15)
-                        .padding(.top, 5)
                     Text("알림시간 설정은 주인장 맘입니다.")
                         .font(.pretendardSemiBold_15)
                         .foregroundColor(Color.logoutColor)
-                        .padding(.top, 5)
                 }
                 .vSpacing(.topLeading)
             }
