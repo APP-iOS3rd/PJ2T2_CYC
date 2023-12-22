bea649ca77992183ee954b9b642a7d4a6e46a18c
Dongho <hamfan524@naver.com>
2023-12-22 15:49:25 +0900

    [Add] keys.swift파일 추가

---
+++ CYC/Login/keys.swift
@@ -0,0 +1,12 @@
+//
+//  keys.swift
+//  CYC
+//
+//  Created by 황성진 on 12/11/23.
+//
+
+import Foundation
+
+let client_id = Bundle.main.infoDictionary?["CLIENT_ID"] ?? ""
+let client_secret = Bundle.main.infoDictionary?["CLIENT_SECRET"] ?? ""
+let redirectURL = "cyc://callback"
